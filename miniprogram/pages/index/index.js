3b6c5c35947632e9f68b984ea68f083c3a8e7f6f
herby <2763431590@qq.com>
2020-11-24 18:03:59 +0800

    增加选择食物页面

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -3,11 +3,11 @@ import util from '../../utils/util.js'
 
 const app = getApp()
 const db = wx.cloud.database()
-var user_openid = ''
-const goods_collection = db.collection('goods')
+const GOODS_COLLECTION = 'goods'
 
 Page({
   data: {
+    
     avatarUrl: './user-unlogin.png',
     userInfo: {},
     logged: false,
@@ -18,10 +18,12 @@ Page({
     goods_list:[],
     his_list: [],
     focue_value: false,
-    hiddenemptypart: true
+    hiddenemptypart: true,
+    user_openid: ''
   },
 
   onLoad: function() {
+    var that = this
     if (!wx.cloud) {
       wx.redirectTo({
         url: '../chooseLib/chooseLib',
@@ -45,7 +47,19 @@ Page({
         }
       }
     })
-    this.onGetOpenid()
+
+    //获取缓存数据，如果缓存有用户openid，则无需调用云函数去获取，节省流量
+    var value = wx.getStorageSync('user_openid')
+    if(value){
+      that.setData({
+        "user_openid": value
+      })
+    }
+    else{
+      //调用云函数获取openid
+      this.onGetOpenid()
+    }
+
   },
 
   onGetUserInfo: function(e) {
@@ -65,13 +79,9 @@ Page({
       data: {},
       success: res => {
         console.log('[云函数] [login] user openid: ', res.result.openid)
-        this.setData({
-          user_openid: res.result.openid
-        })
-        user_openid = res.result.openid
-        // wx.navigateTo({
-        //   url: '../userConsole/userConsole',
-        // })
+        
+        //将获取的openid缓存到本地
+        wx.setStorageSync('user_openid', res.result.openid)
       },
       fail: err => {
         console.error('[云函数] [login] 调用失败', err)
@@ -84,8 +94,8 @@ Page({
    */
   onShow: function() {
     var that = this;
- 
-    db.collection('goods').get({
+    //调用数据库，获取集合的数据
+    db.collection(GOODS_COLLECTION).get({
       success: function(res){
         that.setData({
           'goods_list': res.data
@@ -97,12 +107,30 @@ Page({
         }
       }
     })
+    
+  },
+
+  //封装获取数据库数据的方法
+  customizeGetDB: function(col){
+
+    var that = this;
+    //调用数据库，获取集合的数据
+    db.collection(GOODS_COLLECTION).get({
+      success: function(res){
+        console.log(res.data)
+        that.setData({
+          'goods_list': res.data
+        })
+      }
+    })
+
+
   },
 
   bindmodalinput: function(){
     this.setData({
       //注意到模态框的取消按钮也是绑定的这个函数，
-      //所以这里直接取反hiddenmodalput，也是没有毛病
+      //所以这里直接取反hiddenmodalput
       hiddenmodalput: !this.data.hiddenmodalput,
      
     })
@@ -114,13 +142,19 @@ Page({
     },100)
   
   },
+
+
+  //modal框 cancel
   cancel: function(){
     this.setData({
       hiddenmodalput: true
      });
   },
+
+  //modal框 confirm
   confirm: function(e){
-    
+    var that = this
+
     //名字为空，toast提示且不能保存
     if(this.data.goods_name==''){
       wx.showToast({
@@ -138,66 +172,32 @@ Page({
         hiddenemptypart: true
        })
 
-    // var item = {
-    //   _openid: this.data.user_openid,
-    //   name: this.data.goods_name,
-    //   num: 1,
-    //   create_time:  util.getNowTimestamp()
-    // }
-
-    // var that = this
     try {
-      // db.collection('goods').get({
-      //   success: function(res){
-      //     if(res.data){
-      //       that.setData({
-      //         his_list: res.data
-      //       })
-      //     }
-      //   }
-      // })
 
-      db.collection('goods').add({
-      data: {
+    
+    
+      //添加记录到数据库
+      var item={
         _openid: this.data.openid,
         name: this.data.goods_name,
         num: 1,
         create_time: util.getNowTimestamp()
-      },
-      success: function(res){
-          console.log(res)
       }
+      db.collection(GOODS_COLLECTION).add({
+        data: item,
+        success: function(res){
+          that.data.goods_list.push(item)
+          that.setData({
+            goods_list: that.data.goods_list
+          })
+          console.log(that.data.goods_list)
+        }
       })
-
-    db.collection('goods').get({
-      success: function(res){
-        this.setData({
-          goods_list: res.data
-        })
-      console.log("test"+res.data)
-      }
-    })
       
-      // var value = wx.getStorageSync('goods_list')
-      // if (value) {
-      //   that.setData({
-      //     his_list: value    
-      //   })
-      // }
     } catch (e) {
+      console.log("" +e)
       // Do something when catch error
     }
-
-    // this.data.his_list.push(item)
-    
-    // try {
-    //   this.setData({
-    //     goods_list: this.data.his_list    
-    //   })
-
-
-    //   wx.setStorageSync('goods_list', this.data.his_list)
-    // } catch (e) { }
 
     //输入框不保留上一次的输入记录
     this.setData({
@@ -212,50 +212,61 @@ Page({
   },
 
 
-
-  /**增加物品数量*/
-  addCount(e){
+   /**更新物品数量*/
+   bindChangNum(e){
+    var that = this
     const index = e.currentTarget.dataset.index
     let list = this.data.goods_list
     let num = list[index].num
-    num = num + 1 
-    list[index].num = num
-    this.setData({
-      goods_list: list
-    })
-
-    wx.setStorageSync('goods_list', this.data.goods_list)
-
-  },
-
-   /**减少物品数量*/
-  decreaseCount(e){
-    const index = e.currentTarget.dataset.index
-    let list = this.data.goods_list
-    let num = list[index].num
-    if (num==0){
+    
+    num = num + Number(e.currentTarget.dataset.value)
+    if (num<0){
       // list.splice(index,1)
       return
     }
-    num = num - 1
     list[index].num = num
 
     this.setData({
       goods_list: list
     })
-    wx.setStorageSync('goods_list', this.data.goods_list)
+                          
+    db.collection(GOODS_COLLECTION).where({
+      name: list[index].name,
+      _openid: that.data.user_openid
+
+    }).update({
+      data: {
+        num: num
+      },
+    })
   },
 
   /**删除物品 */
   binddeleteitem(e){
+
+    var that = this
     const index = e.currentTarget.dataset.index
     let list = this.data.goods_list
+    let targetName = list[index].name
     list.splice(index, 1)
     this.setData({
       goods_list: list
     })
-    wx.setStorageSync('goods_list', this.data.goods_list)
 
+    //删除对应的数据（根据名称和openid来查找）
+    db.collection(GOODS_COLLECTION).where({
+      name: targetName,
+      _openid: that.data.user_openid
+    }).remove({
+      success: function(res) {
+        wx.showToast({
+          icon: 'success',
+          title: '删掉了～'
+        })
+        console.log(res.data)
+      }
+    })
+    
     //设置缺省页展示
     if(list.length==0){
       this.setData({
@@ -313,8 +324,5 @@ Page({
       }
     })
   },
-
-
-
 
 })
